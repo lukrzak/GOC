ad3e2bd74ac7799a1fc35c9e9087ea61d0ef7e78
lukrzak <lukrzak@wp.pl>
2023-12-15 22:55:06 +0100

    Initial commit for lobby-backend

---
+++ lobby-backend/src/main/java/com/lukrzak/goc/lobbybackend/LobbyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lukrzak.goc.lobbybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobbyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobbyBackendApplication.class, args);
+	}
+
+}
---
+++ lobby-backend/src/test/java/com/lukrzak/goc/lobbybackend/LobbyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukrzak.goc.lobbybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
