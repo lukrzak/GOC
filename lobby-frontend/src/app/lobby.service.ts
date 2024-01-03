05c2dc3baed244bd147355ecc5bb506849edd63c
lukrzak <lukrzak@wp.pl>
2024-01-04 00:13:00 +0100

    Created frontend service

--- lobby-frontend/src/app/lobby.service.ts
+++ lobby-frontend/src/app/lobby.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Lobby } from './lobby';
 import { Lobbies } from './lobbies';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Player } from './player';
+import { LobbyDetails } from './lobby-details';
 
 @Injectable({
   providedIn: 'root'
@@ -11,30 +12,60 @@ import { Player } from './player';
 export class LobbyService {
 
   private http: HttpClient;
-  private lobbies: Lobby[] = [];
-  private LOBBIES_URI = "http://localhost:8080/api/v1/lobbies";
+  private LOBBIES_URL = "http://localhost:8080/api/v1/lobbies";
 
   constructor(http: HttpClient) {
     this.http = http;
   }
 
   getLobbies(): Observable<Lobbies> {
-    console.log("Sending getLobbies request");
-    return this.http.get<Lobbies>(this.LOBBIES_URI);
+    console.log("Sending get lobbies request: " + this.LOBBIES_URL);
+
+    return this.http.get<Lobbies>(this.LOBBIES_URL);
   }
 
-  getLobby(id: string): Lobby | undefined {
-    return this.lobbies.find(lobby => lobby.id === id);
+  getLobby(id: string): Observable<LobbyDetails> {
+    const GET_LOBBY_URL = this.LOBBIES_URL + "/" + id;
+    console.log("Sending get lobby request: " + GET_LOBBY_URL);
+
+    return this.http.get<LobbyDetails>(GET_LOBBY_URL);
   }
 
-  joinLobby(name: string, lobbyId: string): Observable<any> {
+  createLobby(lobbyName: string, passwordProtected: boolean): Observable<any> {
+    console.log("Sending create lobby request: " + this.LOBBIES_URL);
+    const body = {
+      name: lobbyName,
+      passwordProtected: passwordProtected
+    }
+
+    return this.http.post(this.LOBBIES_URL, body, {responseType: 'text' as 'json'});
+  }
+
+  joinLobby(playerName: string, lobbyId: string): Observable<any> {
     const player: Player = {
-      name: name
+      name: playerName
     };
-    const requestUri = this.LOBBIES_URI + "/join/" + lobbyId;
-    console.log("Sending join lobby request:" + requestUri);
+    const JOIN_REQUEST_URL = this.LOBBIES_URL + "/join/" + lobbyId;
+    console.log("Sending join lobby request:" + JOIN_REQUEST_URL);
 
-    return this.http.post(requestUri, player, {responseType: 'text' as 'json'});
+    return this.http.post(JOIN_REQUEST_URL, player, {responseType: 'text' as 'json'});
+  }
+
+  leaveLobby(playerName: string, lobbyId: string): Observable<any> {
+    const player: Player = {
+      name: playerName
+    };
+    const LEAVE_REQUEST_URL = this.LOBBIES_URL + "/leave/" + lobbyId;
+    console.log("Sending leave lobby request: " + LEAVE_REQUEST_URL);
+
+    return this.http.post(LEAVE_REQUEST_URL, player, {responseType: 'text' as 'json'});
+  }
+
+  deleteLobby(lobbyId: string): Observable<any> {
+    const DELETE_LOBBY_URL = this.LOBBIES_URL + "/" + lobbyId;
+    console.log("Sending delete lobby request: " + DELETE_LOBBY_URL);
+
+    return this.http.delete(DELETE_LOBBY_URL, {responseType: 'text' as 'json'});
   }
 
 }
