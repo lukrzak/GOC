4b330dc1c9cab55e6c4736a8579f392502069695
lukrzak <lukrzak@wp.pl>
2024-01-05 13:40:05 +0100

    Implemented basic player model

---
+++ lobby-backend/src/main/java/com/lukrzak/goc/lobbybackend/player/Player.java
@@ -0,0 +1,19 @@
+package com.lukrzak.goc.lobbybackend.player;
+
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Player {
+
+	private String name;
+
+}
