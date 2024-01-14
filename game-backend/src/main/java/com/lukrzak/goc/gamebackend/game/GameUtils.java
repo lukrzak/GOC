a0349463b963f703106257067aae5f93cacf5f13
lukrzak <lukrzak@wp.pl>
2024-01-14 21:07:36 +0100

    Moved Resource mapper to GameUtils function

--- game-backend/src/main/java/com/lukrzak/goc/gamebackend/game/GameUtils.java
+++ game-backend/src/main/java/com/lukrzak/goc/gamebackend/game/GameUtils.java
@@ -2,9 +2,13 @@ package com.lukrzak.goc.gamebackend.game;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lukrzak.goc.gamebackend.game.country.Resources;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class GameUtils {
 
@@ -20,4 +24,17 @@ public class GameUtils {
 		return node;
 	}
 
+	public static EnumMap<Resources, Float> jsonNodeToResourcesMap(JsonNode node) {
+		EnumMap<Resources, Float> resourcesMap = new EnumMap<>(Resources.class);
+		Iterator<Map.Entry<String, JsonNode>> elements = node.fields();
+
+		while(elements.hasNext()) {
+			Map.Entry<String, JsonNode> entry = elements.next();
+			Resources resource = Resources.valueOf(entry.getKey());
+			float amount = entry.getValue().floatValue();
+			resourcesMap.put(resource, amount);
+		}
+
+		return resourcesMap;
+	}
 }
--- game-backend/src/main/java/com/lukrzak/goc/gamebackend/game/building/BuildingFactory.java
+++ game-backend/src/main/java/com/lukrzak/goc/gamebackend/game/building/BuildingFactory.java
@@ -6,14 +6,12 @@ import com.lukrzak.goc.gamebackend.game.country.Resources;
 
 import java.io.IOException;
 import java.util.EnumMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class BuildingFactory {
 
 	private static final String BUILDING_CONFIG_FILE = "building_config.json";
 
-	static Building createBuilding(BuildingTypes type) {
+	public static Building createBuilding(BuildingTypes type) {
 		BuildingEffect buildingEffect = null;
 		try {
 			buildingEffect = getBuildingEffect(type);
@@ -34,20 +32,13 @@ public class BuildingFactory {
 
 		JsonNode node = GameUtils.getConfigNode(BUILDING_CONFIG_FILE, BUILDING_COST_NODE_NAME, type.toString());
 		if (node == null) {
-			System.err.println("Invalid building type");
+			throw new RuntimeException("Incorrect config json path");
 		}
 
 		float fundsChange = (float) node.get(FUNDS_CHANGE_NODE_NAME).asDouble();
 		int populationChange = node.get(POPULATION_CHANGE_NODE_NAME).asInt();
-		EnumMap<Resources, Float> resourcesChange = new EnumMap<>(Resources.class);
-		Iterator<Map.Entry<String, JsonNode>> elements = node.path(RESOURCES_CHANGE_NODE_NAME).fields();
-
-		while(elements.hasNext()) {
-			Map.Entry<String, JsonNode> entry = elements.next();
-			Resources resource = Resources.valueOf(entry.getKey());
-			float amount = entry.getValue().floatValue();
-			resourcesChange.put(resource, amount);
-		}
+		EnumMap<Resources, Float> resourcesChange
+				= GameUtils.jsonNodeToResourcesMap(node.path(RESOURCES_CHANGE_NODE_NAME));
 
 		return new BuildingEffect(fundsChange, populationChange, resourcesChange);
 	}
