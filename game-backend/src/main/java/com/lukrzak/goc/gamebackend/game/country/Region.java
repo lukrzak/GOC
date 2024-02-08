8c6211039711c64c99dd0e06fd6030f49d4e4de5
lukrzak <lukrzak@wp.pl>
2024-02-08 22:22:13 +0100

    Added constructors. Changed populationChange to float

--- game-backend/src/main/java/com/lukrzak/goc/gamebackend/game/country/Region.java
+++ game-backend/src/main/java/com/lukrzak/goc/gamebackend/game/country/Region.java
@@ -2,6 +2,7 @@ package com.lukrzak.goc.gamebackend.game.country;
 
 import com.lukrzak.goc.gamebackend.game.building.Building;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
@@ -9,14 +10,23 @@ import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Region {
 
 	private int id;
 	private String name;
 	private int population;
-	private int populationChange;
+	private float populationChange;
 	private Country country;
-	private List<Building> buildings = new ArrayList<>();
+	private final List<Building> buildings = new ArrayList<>();
+
+	public Region(int id, String name, int population, float populationChange, Country country) {
+		this.id = id;
+		this.name = name;
+		this.population = population;
+		this.populationChange = populationChange;
+		country.assignRegionToCountry(this);
+	}
 
 	public void finishBuilding(Building building) {
 		buildings.add(building);
